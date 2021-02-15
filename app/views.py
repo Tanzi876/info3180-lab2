e15632f402f2856425dd77cc3bfe7e1a78c48b3a
Tanzi876 <tanyaleelawrence@gmail.com>
2021-02-14 21:22:07 -0500

    Complete

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from datetime import date
 
 ###
 # Routing for your application.
@@ -24,7 +24,16 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+    
 
+def format_date_joined():
+    joined=date.strftime('%m,%Y')
+    print ("Joined",joined)
+
+    
 ###
 # The functions below should be applicable to all Flask apps.
 ###
